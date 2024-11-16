59b60eeeb095b203eba90d47ce643fe2a761b0cd
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2024-11-16 14:45:31 +0000

    questions 1 and 2 done

--- CPPS/perguntas.cpp
+++ CPPS/perguntas.cpp
@@ -24,9 +24,8 @@ int acharMaisAntigo(list<Obras> lista)
 string livroOuFilme(list<Obras> lista)
 {
     int livros = 0, filmes = 0;
-    list<Obras>::iterator it = lista.begin(); // iterador que aponta para o inicio da lista
 
-    for (it; it != lista.end(); ++it)
+    for (list<Obras>::iterator it = lista.begin(); it != lista.end(); ++it)
     {
         if (it->getMidia() == "Livro")
             livros++;
