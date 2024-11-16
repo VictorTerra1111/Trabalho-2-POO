16d75e785e56bde538f61e19c44d9907c1b52bfe
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2024-11-16 14:24:11 +0000

    memory leak solved in acharMaisAntigo

--- CPPS/perguntas.cpp
+++ CPPS/perguntas.cpp
@@ -6,26 +6,35 @@
 
 using namespace std;
 
-int acharMaisAntigo(list<Obras> lista){
+int acharMaisAntigo(list<Obras> lista)
+{
     list<Obras>::iterator it = lista.begin(); // iterador que aponta para o inicio da lista
     int resposta = lista.begin()->getAnoPublicacao();
-    while (it != lista.end()){
-        ++it;
-        int atual = it->getAnoPublicacao();
-        if(resposta > atual) {
-            resposta = atual;
+    while (it != lista.end())
+    {
+        if (resposta > it->getAnoPublicacao())
+        {
+            resposta = it->getAnoPublicacao();
         }
+        ++it;
     }
     return resposta;
 }
-string livroOuFilme(list<Obras> lista){
+
+string livroOuFilme(list<Obras> lista)
+{
     int livros = 0, filmes = 0;
     list<Obras>::iterator it = lista.begin(); // iterador que aponta para o inicio da lista
 
-    for(it; it != lista.end(); ++it){
-        if(it->getMidia() == "Livro") livros++;
-        if(it->getMidia() == "Filme") filmes++;
+    for (it; it != lista.end(); ++it)
+    {
+        if (it->getMidia() == "Livro")
+            livros++;
+        if (it->getMidia() == "Filme")
+            filmes++;
     }
-    if(livros > filmes) return "livros";
-    else return "filmes";
+    if (livros > filmes)
+        return "livros";
+    else
+        return "filmes";
 }
