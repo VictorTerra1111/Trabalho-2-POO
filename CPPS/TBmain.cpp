7dfabbad7d0db68395e624acd81002c719e6558c
Raul Costa Soares <114699737+RaulCostaSoares@users.noreply.github.com>
2024-11-17 17:57:06 -0300

    Update TBmain.cpp

--- CPPS/TBmain.cpp
+++ CPPS/TBmain.cpp
@@ -37,7 +37,7 @@ int main()
 
             break;
         case 4:
-                cout << " caso 4" << endl;
+                cout << "As obras que não são nem filme nem livro são" << endl << nemFilmesNemLivros(lista) <<endl;
 
             break;
         case 5:
